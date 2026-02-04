e5797461092d63becbcce76cea540ee707dc012b
Claudia Castagna <claudiacastagna@Claudias-MacBook-Pro-M4.local>
2026-02-04 16:30:38 -0500

    Adds Mockito dependencies, updates userinterface.txt, begins tests for UserinterfaceTest

---
+++ src/main/java/Userinterface.java
@@ -0,0 +1,29 @@
+public class Userinterface {
+
+
+    public static String noArg() {
+        //print available files
+        return "";
+    }
+
+    public static String oneArg() {
+        //print contents of file using default decipher
+        return "";
+    }
+
+    public static String twoArg() {
+        //print contents of file using specified decipher
+        return "";
+    }
+
+    public static String moreArg() {
+        //Produce error, too many positional arguments
+        return "";
+    }
+
+    public static String determineOutput(int numArgs) {
+        //Determine which output based on numArgs
+        return "";
+    }
+
+}
--- src/test/java/UserinterfaceTest.java
+++ src/test/java/UserinterfaceTest.java
@@ -1,13 +1,50 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class UserinterfaceTest {
 
+    public String availableFiles;
+    public String defaultDecipheredContents;
+    public String specificDecipheredContents;
+
     @BeforeEach
     void setUp() {
+
+        availableFiles = "file1 \n file2 \n file 3";
+        defaultDecipheredContents = "Hello World!";
+        specificDecipheredContents = "Good Morning!";
+
+        Userinterface mockArgs = Mockito.mock(Userinterface.class);
+        Mockito.when(Userinterface.determineOutput(0)).thenReturn(availableFiles);
+        Mockito.when(Userinterface.determineOutput(1)).thenReturn(defaultDecipheredContents);
+        Mockito.when(Userinterface.determineOutput(2)).thenReturn(specificDecipheredContents);
+
+    }
+
+    @Test
+    void determineOutput() {
+
 
 
 
     }
 
+    @Test
+    void noArg() {
+
+    }
+
+    @Test
+    void oneArg() {
+    }
+
+    @Test
+    void twoArg() {
+    }
+
+    @Test
+    void moreArg() {
+    }
 
 }
