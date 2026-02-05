a4a8b23e6c128d4fd3033ca38a99f36115b72ba8
ellie kim <107424488+elliekim2@users.noreply.github.com>
2026-02-04 20:40:43 -0500

    added cipher testing class and main class

---
+++ src/test/java/CipherTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CipherTest {
+
+    @Test
+    void testSimpleDecipher() throws Exception {
+        String input = "bcdea";
+        String expected = "abcde";
+
+      //  String actual = Cipher.decipher(input); (ill uncomment after i code cipher class)
+
+      //  assertEquals(expected, actual);
+    }
+}
