a39985381c34daedbec53cdf1898e8fdc74ccd0c
Claudia Castagna <claudiacastagna@Claudias-MacBook-Pro-M4.local>
2026-02-10 18:39:47 -0500

    Validates user input

--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -1,25 +1,42 @@
+import javax.sound.midi.SysexMessage;
+
 public class Userinterface {
 
 
     public static String determineOutput(String[] arguments) throws IllegalArgumentException {
-        //Determine which output based on numArgs
-        if (arguments.length == 0)  {
+        if (arguments.length == 0) {
             return ProgramControl.listFiles();
         }
-        
-        else if (arguments.length == 1)  {
-            //Also check type of argument?
-            return ProgramControl.retrieve(Integer.parseInt(arguments[0]));
+
+        else if (arguments.length == 1) {
+            try {
+                return ProgramControl.retrieve(Integer.parseInt(arguments[0]));
+            }
+
+            catch (NumberFormatException e) {
+                System.out.println("Error: Please enter the number corresponding to the file you wish to decipher.");
+            }
         }
-        
-        else if (arguments.length == 2)  {
-            //Also check type of argument?
-            return ProgramControl.retrieve(Integer.parseInt(arguments[0]), arguments[1]);
+
+        else if (arguments.length == 2) {
+            try {
+                return ProgramControl.retrieve(Integer.parseInt(arguments[0]), arguments[1]);
+            }
+
+            catch (NumberFormatException e) {
+                System.out.println("Error: First argument must be the number of the file.");
+            }
+            catch (IllegalArgumentException e) { // assuming retrieve throws this for invalid key
+                System.err.println("Error: The key '" + arguments[1] + "' is invalid.");
+            }
         }
-        
+
         else {
             throw new IllegalArgumentException("Attempted to run program with too many arguments.");
         }
+        return "Program failed due to invalid input.";
     }
 
 }
+
+
--- src/test/java/UserinterfaceTest.java
+++ src/test/java/UserinterfaceTest.java
@@ -9,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class UserinterfaceTest {
 
     MockedStatic<ProgramControl> pcMock;
+    String[] test1;
+    String[] test2;
 
 
     @BeforeEach
@@ -16,40 +18,27 @@ class UserinterfaceTest {
         pcMock = Mockito.mockStatic(ProgramControl.class);
         pcMock.when(ProgramControl::listFiles).thenReturn("file1 \n file2 \n file3");
         pcMock.when(() -> ProgramControl.retrieve(1)).thenReturn("The file contents deciphered with default cipher");
-        pcMock.when(() -> ProgramControl.retrieve(1, "Special cipher")).thenReturn("The file contents with specified cipher");
-    }
+        pcMock.when(() -> ProgramControl.retrieve(1, "Special key")).thenReturn("The file contents with specified cipher");
 
+        test1 = new String[1];
+        test1[0] = "01";
+
+        test2 = new String[2];
+        test2[0] = "02";
+        test2[1] = "key1";
+
+
+
+
+    }
 
     @Test
     void determineOutput() {
 
-        assertEquals(Userinterface.determineOutput(0), ProgramControl.listFiles());
+        assertEquals(Userinterface.determineOutput(test1), ProgramControl.listFiles());
         assertEquals(Userinterface.determineOutput(1), ProgramControl.retrieve(1));
-        assertEquals(Userinterface.determineOutput(2), ProgramControl.retrieve(1, "Special cipher"));
+        assertEquals(Userinterface.determineOutput(2), ProgramControl.retrieve(1, "Special key"));
         assertThrows(IllegalArgumentException.class, () -> Userinterface.determineOutput(-1));
 
     }
-
-    @Test
-    void displayFiles() {
-        try (MockedStatic<ProgramControl> pcMock = Mockito.mockStatic(ProgramControl.class)) {
-            assertEquals(Userinterface.displayFiles(), ProgramControl.listFiles());
-        }
-
     }
-
-    @Test
-    void decipherDefault() {
-        try (MockedStatic<ProgramControl> pcMock = Mockito.mockStatic(ProgramControl.class)) {
-            assertEquals(Userinterface.decipherDefault(), ProgramControl.retrieve(1));
-        }
-    }
-
-    @Test
-    void decipherSpecified() {
-        try (MockedStatic<ProgramControl> pcMock = Mockito.mockStatic(ProgramControl.class)) {
-            assertEquals(Userinterface.decipherSpecified(), ProgramControl.retrieve(1, "Special cipher"));
-        }
-    }
-
-}
