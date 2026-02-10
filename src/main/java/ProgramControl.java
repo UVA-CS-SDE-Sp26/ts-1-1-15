0fdb13b8e2eb5bb2653772e147455e481c7d3648
dinomickeyHidden <89482457+dinomickeyHidden@users.noreply.github.com>
2026-02-10 12:57:46 -0500

    updated ProgramControl.java

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -4,7 +4,8 @@ public class ProgramControl {
     //return list of files, from Team Member B, Nathan Wattier
     public static String listFiles(){
         StringBuilder out= new StringBuilder();
-        ArrayList<String> list = FileHandler.getFilenames();
+        FileHandler handler = new FileHandler();
+        ArrayList<String> list = handler.getFilenames();
         for (int i=0; i < list.size(); ++i){
             out.append(String.format("%02d", (i + 1))).append(" ").append(list.get(i)).append("\n");
         }
@@ -13,16 +14,18 @@ public class ProgramControl {
 
     //return contents of file using default key
     public static String retrieve(int num){
-        String filename = FileHandler.getFilenames().get(num);
-        String contents = FileHandler.getContents(filename);
+        FileHandler handler = new FileHandler();
+        String filename = handler.getFilenames().get(num);
+        String contents = handler.getContents(filename);
         return Cipher.decipher(contents);
     }
 
     //return contents of file corresponding to the int num,
     //deciphered from Team Member D, Ellie Kim
     public static String retrieve(int num, String key){
-        String filename = FileHandler.getFilenames().get(num);
-        String contents = FileHandler.getContents(filename);
+        FileHandler handler = new FileHandler();
+        String filename = handler.getFilenames().get(num);
+        String contents = handler.getContents(filename);
         return Cipher.decipher(contents, key);
     }
 }
