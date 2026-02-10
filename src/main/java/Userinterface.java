d57ee17b8a2f4c8ea8481134c9dfe95362a77502
Claudia Castagna <claudiacastagna@Claudias-MacBook-Pro-M4.local>
2026-02-10 18:14:56 -0500

    Updates userinterface

--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -1,35 +1,20 @@
 public class Userinterface {
 
 
-    public static String displayFiles() {
-        //print available files
-        return ProgramControl.listFiles();
-    }
-
-    public static String decipherDefault() {
-        //print contents of file using default decipher
-
-    }
-
-    public static String decipherSpecified() {
-        //print contents of file using specified decipher
-        return "";
-    }
-
-    public static String determineOutput(int numArgs) throws IllegalArgumentException {
+    public static String determineOutput(String[] arguments) throws IllegalArgumentException {
         //Determine which output based on numArgs
-        if (numArgs == 0)  {
-            return displayFiles();
+        if (arguments.length == 0)  {
+            return ProgramControl.listFiles();
         }
         
-        else if (numArgs == 1)  {
+        else if (arguments.length == 1)  {
             //Also check type of argument?
-            return decipherDefault();
+            return ProgramControl.retrieve(Integer.parseInt(arguments[0]));
         }
         
-        else if (numArgs == 2)  {
+        else if (arguments.length == 2)  {
             //Also check type of argument?
-            return decipherSpecified();
+            return ProgramControl.retrieve(Integer.parseInt(arguments[0]), arguments[1]);
         }
         
         else {
