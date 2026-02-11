c30fd7d7516af1d240fc14ec210175961b43eb16
dinomickeyHidden <89482457+dinomickeyHidden@users.noreply.github.com>
2026-02-11 15:50:09 -0500

    updated programcontrol.java

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -3,30 +3,45 @@ import java.util.ArrayList;
 
 public class ProgramControl {
     //return list of files, from Team Member B, Nathan Wattier
-    public static String listFiles() throws IOException {
+    public static String listFiles() {
         StringBuilder out= new StringBuilder();
         FileHandler handler = new FileHandler();
-        ArrayList<String> list = handler.getFilenames();
-        for (int i=0; i < list.size(); ++i){
-            out.append(String.format("%02d", (i + 1))).append(" ").append(list.get(i)).append("\n");
+        try {
+            ArrayList<String> list = handler.getFilenames();
+            for (int i=0; i < list.size(); ++i){
+                out.append(String.format("%02d", (i + 1))).append(" ").append(list.get(i)).append("\n");
+            }
+            return out.toString();
         }
-        return out.toString();
+        catch (Exception e){
+            return "Error: "+e;
+        }
     }
 
     //return contents of file using default key
     public static String retrieve(int num) throws IOException {
         FileHandler handler = new FileHandler();
-        String filename = handler.getFilenames().get(num);
-        String contents = handler.getContents(filename);
-        return Cipher.decipher(contents);
+        try{
+            String filename = handler.getFilenames().get(num);
+            String contents = handler.getContents(filename);
+            return Cipher.decipher(contents);
+        }
+        catch (Exception e){
+            return "Error: "+e;
+        }
     }
 
     //return contents of file corresponding to the int num,
     //deciphered from Team Member D, Ellie Kim
     public static String retrieve(int num, String key) throws IOException {
         FileHandler handler = new FileHandler();
-        String filename = handler.getFilenames().get(num);
-        String contents = handler.getContents(filename);
-        return Cipher.decipher(contents, key);
+        try {
+            String filename = handler.getFilenames().get(num);
+            String contents = handler.getContents(filename);
+            return Cipher.decipher(contents, key);
+        }
+        catch (Exception e){
+            return "Error: "+e;
+        }
     }
 }
